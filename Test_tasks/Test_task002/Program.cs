fb8ffc65db540ba432b500c280045feb3c0fe178
Holofiber <aminus2010@gmail.com>
2017-07-15 17:44:39 +0300

    Add second task

---
+++ Test_tasks/Test_task002/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4;
+            Console.Write("Enter first number : ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third number : ");
+            n3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter four number : ");
+            n4 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The average : {0}", (n1 + n2 + n3 + n4) / 4);
+
+            Console.Read();
+        }
+    }
+}
