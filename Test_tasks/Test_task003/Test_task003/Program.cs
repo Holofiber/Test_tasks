56f1b75aa11d2fd275bed4015761044125e628c8
Holofiber <aminus2010@gmail.com>
2017-07-16 10:07:30 +0300

    Add test task003

---
+++ Test_tasks/Test_task003/Test_task003/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.Write("Enter first number : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third number : ");
+            z = Convert.ToInt32(Console.ReadLine());
+            int result1 = (x + y) * z, result2 = x * y + y * z;
+            Console.Write("Result of specified numbers {0} , {1} and {2} , (x+y)*z is {3} and x*y+y*z is {4}", x, y, z, result1, result2);
+            Console.Read();
+        }
+    }
+}
