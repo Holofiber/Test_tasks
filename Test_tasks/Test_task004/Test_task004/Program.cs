1debf728752fb8d4a69bfcac21f9041436e38fe7
Holofiber <aminus2010@gmail.com>
2017-07-18 00:27:13 +0300

    Add_test task004

---
+++ Test_tasks/Test_task004/Test_task004/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+             a = Convert.ToInt16(Console.ReadLine());
+            if (a >= 1 && a < 100000)
+            {
+                if (a % 2 != 0) ;
+
+                Console.WriteLine("Washinghton");
+            }
+
+            else
+            {
+                Console.WriteLine("Hollywood");
+            }
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
