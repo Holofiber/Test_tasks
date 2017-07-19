b08398b598107f3b411c614cc487f66441e9083b
Holofiber <aminus2010@gmail.com>
2017-07-20 00:13:28 +0300

    Add test task input 32 5 -56 output 2 5 6

---
+++ Test_tasks/Test_task5/Test_task5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            k = Convert.ToInt32(Console.ReadLine());
+            while (k <= 0)
+                k += 10;
+            while (k > 10)
+                k -= 10;
+            Console.WriteLine(k);
+        }
+    }
+}
