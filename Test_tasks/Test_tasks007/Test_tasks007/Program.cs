9aa898b6e8701b27c87fe3b4c19fb40e9e7dc477
Holofiber <aminus2010@gmail.com>
2017-07-26 22:29:03 +0300

    Create matrix n*n

---
+++ Test_tasks/Test_tasks007/Test_tasks007/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_tasks007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, l;
+            Console.WriteLine("Enter your first parametr: ");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second parametr: ");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Matrix {0}*{1} : ", k,l);
+            int[,] myMatrix;
+            myMatrix = new int[k,l];
+       
+           for (int i = 0; i < k; i++)
+            
+                for (int j =0; j < l; j++)
+                   myMatrix[i, j] = i * j;
+
+            for (int i = 0; i < k;i++)
+            {
+                for (int j = 0; j < l; j++)
+                   
+                    Console.Write(myMatrix[i,j] + "\t");
+                Console.WriteLine();
+                    }
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+        }
+        
+    }
+}
