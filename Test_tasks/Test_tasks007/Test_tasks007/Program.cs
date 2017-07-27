eae424c65e5d1199949657ff624ab0bc16b3a5aa
kalashn1k0v <nazar.kalashnikov@gmail.com>
2017-07-28 00:41:19 +0300

    Show example with matrix

--- Test_tasks/Test_tasks007/Test_tasks007/Program.cs
+++ Test_tasks/Test_tasks007/Test_tasks007/Program.cs
@@ -1,41 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test_tasks007
 {
+    public class Matrix
+    {
+        private readonly int k;
+        private readonly int l;
+
+        public int[,] myMatrix;
+        
+        public Matrix(int k, int l)
+        {
+            this.k = k;
+            this.l = l;
+            this.myMatrix = new int[k, l];
+        }
+        
+
+        public void Print()
+        {
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < l; j++)
+                {
+                    var formatedValue = myMatrix[i, j] + "\t";
+                    Console.Write(formatedValue);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void Fill()
+        {
+            for (int i = 0; i < k; i++)
+            for (int j = 0; j < l; j++)
+                myMatrix[i, j] = i * j;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
             int k, l;
+            ReadMatrixParameters(out k, out l);
+            
+            Matrix m1 = new Matrix(k, l);
+            Matrix m2 = new Matrix(k+1, l+1);
+
+
+            m1.Print();
+
+            Console.WriteLine();
+
+            m2.Print();
+
+
+
+
+            //FillMatrix(k, l, myMatrix);
+            
+            //PrintMatrix(k, l, myMatrix);
+
+            
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        
+        private static void ReadMatrixParameters(out int k, out int l)
+        {
             Console.WriteLine("Enter your first parametr: ");
             k = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter your second parametr: ");
             l = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your Matrix {0}*{1} : ", k,l);
-            int[,] myMatrix;
-            myMatrix = new int[k,l];
-       
-           for (int i = 0; i < k; i++)
-            
-                for (int j =0; j < l; j++)
-                   myMatrix[i, j] = i * j;
-
-            for (int i = 0; i < k;i++)
-            {
-                for (int j = 0; j < l; j++)
-                   
-                    Console.Write(myMatrix[i,j] + "\t");
-                Console.WriteLine();
-                    }
-            Console.WriteLine();
-            Console.ReadKey();
-
-
+            Console.WriteLine("Your Matrix {0}*{1} : ", k, l);
         }
+
+
         
     }
 }
