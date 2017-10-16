2afd917896d7c4950a87e83c2da78cd496f45644
Holofiber <aminus2010@gamil.com>
2017-10-16 23:22:39 +0300

    Matrix: Break cod in to function

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -6,84 +6,34 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public class MatrixSize
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int m;
 
+            int format = InputParametrs();
+            MatrixSize matrixSize = InputMatrixSize();
 
-            //InputParametrs
-           
-            
             int[,] myMatrix;
-            myMatrix = new int[k, l];
-            int z = l;
-            for (int i = 0; i < k; i++)
+            myMatrix = new int[matrixSize.Rows, matrixSize.Columns];
+                        
+            FillMatrix(matrixSize, myMatrix);
 
-                for (int j = 0; j < l; j++)
-                    myMatrix[i, j] = i * j;
+            BuildTopOfMatrix(format, matrixSize);
+            
+            BuildVerticalLines(matrixSize, format, myMatrix);
 
-            for (int i = 0; i < k; i++)
-            {
-                for (int j = 0; j < l; j++) ;
-
-            }
-
-            //Вверх матриці, будується один раз
-            Console.Write("╔"+TakeStringNtimes("═", format));
-            for (int n = 0; n < (k - 1); n++)
-                Console.Write("╦"+ TakeStringNtimes("═", format));
-            Console.Write("╗");
-            Console.WriteLine();
-
-
-            //Вертикальні рядки матриці
-            for (m = 0; m < (l - 1); m++)
-            {
-                Console.Write("║");
-                for (int i = 0; i < k; i++)
-                {
-                    for (int j = 0; j < l; j++)
-                        myMatrix[i, j] = i * j;
-                    Console.Write($"{myMatrix[i, m].ToString().PadLeft(format)}" + "║");
-                }
-
-                //
-                Console.WriteLine();
-                Console.Write("╠"+ TakeStringNtimes("═", (format/2+1)));
-                for (int n = 0; n < (k - 1); n++)
-                    Console.Write(TakeStringNtimes("═", (format / 2)) + "╬"+ TakeStringNtimes("═", (format / 2+1)));
-                Console.Write(TakeStringNtimes("═", (format / 2)) + "╣");
-                Console.WriteLine();
-            }
-
-            // Останній рядок
-
-            Console.Write("║");
-            for (int i = 0; i < k; i++)
-            {
-                for (int j = 0; j < l; j++)
-                    myMatrix[i, j] = i * j;
-                Console.Write($"{myMatrix[i, (l - 1)].ToString().PadLeft(format)}" + "║");
-            }
-            Console.WriteLine();
-
-            //Низ матриці будується один раз
-            Console.Write("╚"+TakeStringNtimes("═", format));
-            for (int n = 0; n < (k - 1); n++)
-                Console.Write("╩"+ TakeStringNtimes("═", format));
-            Console.Write("╝");
-            Console.WriteLine();
-
-
-
-
-
-
-
-       
-
-
+            BuildLastLine(format, myMatrix, matrixSize);
+            
+            BuildUnderLine(format, matrixSize);
+            
             Console.WriteLine();
             Console.ReadKey();
 
@@ -96,34 +46,120 @@ namespace ConsoleApp1
             Console.WriteLine(newString);
         }
 
-        private static void InpurParametrs ()
-            {
-            int k = 3, l = 3, m = 0, format = 5;
-            Console.WriteLine("Enter your first parametr: ");
-            k = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter your second parametr: ");
-            l = Convert.ToInt32(Console.ReadLine());
+        private static int InputParametrs()
+        {
+            int format;
+
             Console.WriteLine("Format");
             format = Convert.ToInt32(Console.ReadLine());
 
-
+            return format;
         }
 
-
-        private static void InputParametrs()
+        private static MatrixSize InputMatrixSize()
         {
+            MatrixSize size = new MatrixSize();
 
+            Console.WriteLine("Enter columns: ");
+            size.Columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter rows: ");
+            size.Rows = Convert.ToInt32(Console.ReadLine());
+
+            return size;
         }
+
+
+
 
         private static string TakeStringNtimes(string input, int count)
         {
             string result = String.Empty;
-           
+
             for (int i = 0; i < count; i++)
             {
                 result += input;
             }
             return result;
         }
+
+
+        private static void FillMatrix(MatrixSize size, int[,] matrix)
+        {
+
+            for (int i = 0; i < size.Rows; i++)
+            {
+                for (int j = 0; j < size.Columns; j++)
+                {
+                    matrix[i, j] = i * j;
+                }
+            }
+
+        }
+
+        private static void BuildTopOfMatrix(int format, MatrixSize matrixSize)
+        {
+            Console.Write("╔" + TakeStringNtimes("═", format));
+            for (int n = 0; n < (matrixSize.Rows - 1); n++)
+            {
+                Console.Write("╦" + TakeStringNtimes("═", format));
+            }
+            Console.Write("╗");
+            Console.WriteLine();
+        }
+        
+        private static void BuildVerticalLines(MatrixSize matrixSize,  int format, int[,] matrix)
+        {
+            int m;
+            for (m = 0; m < (matrixSize.Columns - 1); m++)
+            {
+                Console.Write("║");
+                for (int i = 0; i < matrixSize.Rows; i++)
+                {
+                    for (int j = 0; j < matrixSize.Columns; j++)
+                    {
+                        matrix[i, j] = i * j;
+                    }
+                    Console.Write($"{matrix[i, m].ToString().PadLeft(format)}" + "║");
+                }
+
+                //
+                Console.WriteLine();
+                Console.Write("╠" + TakeStringNtimes("═", (format / 2 + 1)));
+                for (int n = 0; n < (matrixSize.Rows - 1); n++)
+                {
+                    Console.Write(TakeStringNtimes("═", (format / 2)) + "╬" + TakeStringNtimes("═", (format / 2 + 1)));
+                }
+                Console.Write(TakeStringNtimes("═", (format / 2)) + "╣");
+                Console.WriteLine();
+            }
+        }
+
+        private static void BuildLastLine (int format, int[,] matrix, MatrixSize matrixSize )
+        {
+            Console.Write("║");
+            for (int i = 0; i < matrixSize.Rows; i++)
+            {
+                for (int j = 0; j < matrixSize.Columns; j++)
+                {
+                    matrix[i, j] = i * j;
+                }
+                Console.Write($"{matrix[i, (matrixSize.Columns - 1)].ToString().PadLeft(format)}" + "║");
+            }
+            Console.WriteLine();
+        }
+
+        private static void BuildUnderLine(int format, MatrixSize matrixSize)
+        {
+            Console.Write("╚" + TakeStringNtimes("═", format));
+            for (int n = 0; n < (matrixSize.Rows - 1); n++)
+            {
+                Console.Write("╩" + TakeStringNtimes("═", format));
+            }
+            Console.Write("╝");
+            Console.WriteLine();
+
+        }
+
     }
 }
